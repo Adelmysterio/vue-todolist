90456ba6a974ac049ef97a0a13a578017a8ad920
AdelMysterio <storliniadelmo@gmail.com>
2024-05-30 13:55:29 +0200

    implementato fino a milestone 2

--- js/script.js
+++ js/script.js
@@ -4,13 +4,45 @@ createApp({
 
     data() {
         return {
-            
+            toDoList: [
+                {
+                    content: "Compare un lanciafiamme",
+                    done: false
+                },
+                {
+                    content: "Accarezzare un cano",
+                    done: true
+                },
+                {
+                    content: "Invadere uno Stato Sovrano",
+                    done: false
+                },
+                {
+                    content: "Vincere Miss Universo",
+                    done: false
+                },
+                {
+                    content: "Confutare la teoria della relatività",
+                    done: false
+                },
+            ],
 
         }
     },
 
     methods: {
-    
+        removeTask: function (i) {
+            this.toDoList.splice(i, 1)
+        },
+
+        addNewTask: function (newContent) {
+            let newTask = {
+                content: newContent,
+                done: false
+            };
+        }
     }
+
+
 
 }).mount('#app')
